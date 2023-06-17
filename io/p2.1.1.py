d0015aa416f98c51785700010fb93abaf7c77d3a
jja2031 <ganghuig45@gmail.com>
2023-06-17 12:29:11 +0900

    :memo:update

---
+++ io/p2.1.1.py
@@ -0,0 +1,9 @@
+hello = "안녕하세요"
+num1 = 15
+num2 = 12
+result = num1 - num2
+school = "한국대학교"
+name = "홍길동"
+A = 0+5
+
+print(A)
---
+++ io/p2.5.2.py
@@ -0,0 +1,4 @@
+year = 2023
+month = 6
+day = 15
+print(year,month,day,sep="/")
